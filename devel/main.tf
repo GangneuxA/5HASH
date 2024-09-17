e75c29de584f0c99804423c015fce947053e6fc4
GangneuxA <gangneux.alex@gmail.com>
2024-09-17 16:16:53 +0200

    add architecture

--- devel/main.tf
+++ devel/main.tf
@@ -1,13 +1,36 @@
+module "mysql" {
+  source = "../modules/mysql"
+  network_name          = var.network
+  container_name_prefix = var.prefix
+  mysql_root_password   = var.mysql_root_password
+  mysql_user            = var.mysql_user
+  mysql_password        = var.mysql_password
+  mysql_database        = var.mysql_database
+  mysql_replica_count   = 1
+}
+
 module "prestashop" {
   source = "../modules/prestashop"
 
-  prestashop_version    = "1.7"
-  network_name          = "my_prestashop_network"
-  container_name_prefix = "myshop"
-  mysql_root_password   = "root_password"
-  mysql_user            = "prestashop_user"
-  mysql_password        = "prestashop_password"
-  mysql_database        = "prestashop_db"
-  replica_count         = 3
+  prestashop_version    = "8.1-apache"
+  network_name          = module.mysql.docker_network_name
+  container_name_prefix = var.prefix
+  mysql_root_password   = var.mysql_root_password
+  mysql_user            = var.mysql_user
+  mysql_password        = var.mysql_password
+  mysql_database        = var.mysql_database
+  mysql_host_name       = module.mysql.mysql_container_name
+  replica_count         = 1
+  admin_mail            = var.admin_mail
+  admin_passwd          = var.admin_passwd
+}
+
+module "ngnix" {
+  source = "../modules/ngnix"
+
+  network_name          = module.mysql.docker_network_name
+  container_name_prefix = var.prefix
+  replica_count         = 1
   load_balancer_port    = 8080
+
 }
---
+++ devel/variables.tf
@@ -0,0 +1,51 @@
+variable "prefix" {
+  description = "Préfixe pour nommer les conteneurs"
+  type        = string
+  default     = "devel"
+}
+
+variable "network" {
+  description = "Docker network"
+  type        = string
+  default     = "my_prestashop_network"
+}
+
+variable "mysql_root_password" {
+  description = "Sensitive Root password pour bdd mysql"
+  type        = string
+  default     = "root_password"
+  sensitive = true
+}
+
+variable "mysql_user" {
+  description = "User pour bdd mysql"
+  type        = string
+  default     = "prestashop_user"
+}
+
+variable "mysql_password" {
+  description = "Sensitive user password pour bdd mysql"
+  type        = string
+  default     = "prestashop_password"
+  sensitive = true
+}
+
+variable "mysql_database" {
+  description = "Nom bdd pour bdd mysql"
+  type        = string
+  default     = "prestashop_db"
+}
+
+variable "admin_mail" {
+  description = "Address mail administrateur"
+  type        = string
+  default     = "admin@admin.fr"
+}
+
+variable "admin_passwd" {
+  description = "password administrateur"
+  type        = string
+  default     = "admin_passwd"
+  sensitive = true
+}
+
